133971008a15d476db235965680c066c335f3e64
xieheng <xieheng84@163.com>
2025-01-21 22:56:54 +0800

    Min、Max和Abs函数

---
+++ Math/Fixed16/Mathf.cs
@@ -0,0 +1,30 @@
+﻿namespace Lwkit.Fixed
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Mathf
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fixed16 Min(Fixed16 a, Fixed16 b)
+        {
+            return a < b ? a : b;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fixed16 Max(Fixed16 a, Fixed16 b)
+        {
+            return a > b ? a : b;
+        }
+    }
+}
---
+++ Math/Fixed32/Mathf.cs
@@ -0,0 +1,42 @@
+﻿namespace Lwkit.Fixed
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Mathf
+    {
+        public static Fixed32 Abs(Fixed32 n)
+        {
+            if (n == Fixed32.MinValue)
+            {
+                return Fixed32.MaxValue;
+            }
+
+            // branchless implementation, see http://www.strchr.com/optimized_abs_function
+            var mask = n.value >> 63;
+            return Fixed32.From((n.value + mask) ^ mask);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fixed32 Min(Fixed32 a, Fixed32 b)
+        {
+            return a < b ? a : b;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fixed32 Max(Fixed32 a, Fixed32 b)
+        {
+            return a > b ? a : b;
+        }
+    }
+}
