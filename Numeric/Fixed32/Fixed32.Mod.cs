8b78b1cfe7a44c548e2044c005b324efa6ecd9ef
xieheng <xieheng84@163.com>
2025-01-23 00:35:42 +0800

    取模

---
+++ Numeric/Fixed32/Fixed32.Mod.cs
@@ -0,0 +1,112 @@
+﻿namespace Lwkit.Fixed
+{
+    /// <summary>
+    /// 定点数 - 取余
+    /// </summary>
+    public partial struct Fixed32
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fixed32 operator %(Fixed32 a, int b)
+        {
+            return Mod(a.value, (long)b << INTEGRAL_BITS);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fixed32 operator %(int a, Fixed32 b)
+        {
+            return Mod((long)a << INTEGRAL_BITS, b.value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fixed32 operator %(Fixed32 a, float b)
+        {
+            return Mod(a.value, (long)(b * FRACTIONAL_MULTIPLIER));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fixed32 operator %(float a, Fixed32 b)
+        {
+            return Mod((long)(a * FRACTIONAL_MULTIPLIER), b.value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fixed32 operator %(Fixed32 a, double b)
+        {
+            return Mod(a.value, (long)(b * FRACTIONAL_MULTIPLIER));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fixed32 operator %(double a, Fixed32 b)
+        {
+            return Mod((long)(a * FRACTIONAL_MULTIPLIER), b.value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fixed32 operator %(Fixed32 a, Fixed32 b)
+        {
+            return Mod(a.value, b.value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private static Fixed32 Mod(long a, long b)
+        {
+            if (a == NaN.value || 
+                b == NaN.value)
+            {
+                return NaN;
+            }
+
+            // 整数部分
+            var aint = a >> INTEGRAL_BITS;
+            var bint = b >> INTEGRAL_BITS;
+            // 小数部分
+            var afrac = (a & FRACTIONAL_MASK);
+            var bfrac = (b & FRACTIONAL_MASK);
+
+            var mint = (aint % bint) << INTEGRAL_BITS;
+            var mfrac = afrac % bfrac;
+
+            return From(mint + mfrac);
+        }
+    }
+}
