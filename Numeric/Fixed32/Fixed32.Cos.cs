3ca39179116c7e0c4e97a68f8b0adf316d86d305
xieheng <xieheng84@163.com>
2025-03-20 15:28:56 +0800

    反余弦

--- Numeric/Fixed32/Fixed32.Cos.cs
+++ Numeric/Fixed32/Fixed32.Cos.cs
@@ -6,7 +6,7 @@
     public partial struct Fixed32
     {
         /// <summary>
-        /// 
+        /// 余弦
         /// </summary>
         /// <param name="radian"></param>
         /// <returns></returns>
@@ -16,13 +16,31 @@
         }
 
         /// <summary>
-        /// 
+        /// 快速计算余弦
         /// </summary>
         /// <param name="radian"></param>
         /// <returns></returns>
         public static Fixed32 FastCos(Fixed32 radian)
         {
             return FastSin(radian + Half_PI);
+        }
+
+        /// <summary>
+        /// 反余弦
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Fixed32 Acos(Fixed32 value)
+        {
+            if (value < NegativeOne || value > One)
+            {
+                throw new ArgumentOutOfRangeException("Must between NegativeOne and One", "value");
+            }
+
+            if (value.rawvalue == 0) return Half_PI;
+
+            var result = Atan(Sqrt(One - value * value) / value);
+            return value.rawvalue < 0 ? result + PI : result;
         }
     }
 }
