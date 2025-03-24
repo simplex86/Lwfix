b3253b371789a3b650af96efee8dc4be8a5f9cfe
xieheng <xieheng84@163.com>
2025-03-24 18:47:08 +0800

    乘法及溢出

--- Fixed32/Numeric/Fixed32.Mul.cs
+++ Fixed32/Numeric/Fixed32.Mul.cs
@@ -14,7 +14,9 @@
         public static Fixed32 operator *(Fixed32 a, int b)
         {
             if (a.IsNaN()) return NaN;
-            return Mul(a.rawvalue, (long)b << INTEGRAL_BITS);
+
+            var result = Mul(a.rawvalue, (long)b << INTEGRAL_BITS, out var _);
+            return FromRaw(result);
         }
 
         /// <summary>
@@ -37,29 +39,76 @@
         public static Fixed32 operator *(Fixed32 a, Fixed32 b)
         {
             if (a.IsNaN() || b.IsNaN()) return NaN;
-            return Mul(a.rawvalue, b.rawvalue);
+
+            var result = Mul(a.rawvalue, b.rawvalue, out var _);
+            return FromRaw(result);
         }
 
-        /// <summary>
-        /// 
+        /// <summarys
+        /// 计算乘法，并获知乘结果是否溢出
         /// </summary>
         /// <param name="a"></param>
         /// <param name="b"></param>
+        /// <param name="overflow"></param>
         /// <returns></returns>
-        private static Fixed32 Mul(long a, long b)
+        private static long Mul(long a, long b, out bool overflow)
         {
-            // 整数部分
-            var aint = a >> INTEGRAL_BITS;
-            var bint = b >> INTEGRAL_BITS;
-            // 小数部分
-            var afrac = (ulong)(a & FRACTIONAL_MASK);
-            var bfrac = (ulong)(b & FRACTIONAL_MASK);
+            overflow = false;
+            if (a == 0 || b == 0) return 0;
 
-            var rint = (aint * bint) << FRACTIONAL_BITS; // 整数的积
-            var rfrac = (afrac * bfrac) >> FRACTIONAL_BITS; // 小数的积
-            var rcrs = (aint * (long)bfrac + bint * (long)afrac); // 交叉部分
+            var aint = a >> FRACTIONAL_BITS;          // xhi
+            var bint = b >> FRACTIONAL_BITS;          // yhi
+            var afrac = (ulong)(a & FRACTIONAL_MASK); // xlo
+            var bfrac = (ulong)(b & FRACTIONAL_MASK); // ylo
 
-            return FromRaw(rint + rcrs + (long)rfrac);
+            var term1 = aint * bint;        // hihi
+            var term2 = aint * (long)bfrac; // hilo
+            var term3 = bint * (long)afrac; // lohi
+            var term4 = afrac * bfrac;      // lolo
+
+            var r = OverflowAdd((long)(term4 >> FRACTIONAL_BITS), term3, ref overflow);
+            r = OverflowAdd(r, term2, ref overflow);
+            r = OverflowAdd(r, term1 << INTEGRAL_BITS, ref overflow);
+
+            var signs = ((a ^ b) & long.MinValue) == 0;
+            if (signs)
+            {
+                if (r < 0 || (overflow && a > 0)) return PositiveInfinity.rawvalue;
+            }
+            else
+            {
+                if (r > 0) return NegativeInfinity.rawvalue;
+            }
+
+            var carry = term1 >> FRACTIONAL_BITS;
+            if (carry != 0 && carry != -1 /*&& xl != -17 && b != -17*/)
+            {
+                return signs ? PositiveInfinity.rawvalue : NegativeInfinity.rawvalue;
+            }
+
+            if (!signs)
+            {
+                var pos = 0L;
+                var neg = 0L;
+
+                if (a > b)
+                {
+                    pos = a;
+                    neg = b;
+                }
+                else
+                {
+                    pos = b;
+                    neg = a;
+                }
+
+                if (r > neg && neg < NegativeOne.rawvalue && pos > One.rawvalue)
+                {
+                    return NegativeInfinity.rawvalue;
+                }
+            }
+
+            return r;
         }
     }
 }
